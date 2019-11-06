85f1da31c15d8fd5755084e18a2673071ad4dce9
Andy Davis <AndrewMarkham@seznam.cz>
2019-11-06 09:21:09 +0100

    wednesday update

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,11 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link rel="stylesheet" href="css/app.css">
+    {{-- <link rel="stylesheet" href="css/app.css"> --}}
 
     <title>MI6 system</title>
 </head>
 <body>
  
-    @yield('contenapp.cs
+    @yield('content')
 </html>
